7c2752da0438c38f11d1a50794224d572523ac5b
David Fazekas <fazekasda@gmail.com>
2016-03-09 12:04:22 +0000

    postgres fixed

--- postgresql/init.sh
+++ postgresql/init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/lib/postgresql/9.4/bin/postgres -D /DATA/postgresql -c config_file=/etc/postgresql/9.4/main/postgresql.conf
+/usr/lib/postgresql/9.4/bin/postgres -D /DATA/postgresql -c config_file=/CONFIG/postgresql/postgresql.conf
